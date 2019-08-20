b3bad3c30a2982751fd2b86cc35f1cb37b6e5959
Sarah Bullard Nakdali <scadmusbullard@gmail.com>
2019-08-20 02:13:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,13 +11,13 @@ end
 
 def using_pop(array)
   continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  continents.pop
+  continent.size == continents.pop
 end
 
 
 def pop_with_args(array)
   dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-  dog_breeds.pop(0,1)
+  dog_breeds.pop(2)
 end
 
 def using_shift(array)
